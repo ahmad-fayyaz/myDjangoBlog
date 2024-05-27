23ee126794da6b0d15b17be63d224c8ba63a0638
afayyaz1 <afayyaz1@swarthmore.edu>
2024-05-26 20:52:49 -0400

    added article titles through shell

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -7,3 +7,7 @@ class Article(models.Model):
     date = models.DateTimeField(auto_now_add=True)
     #add thumbnail later
     #add author later
+    def __str__(self):
+        return self.title
+
+
