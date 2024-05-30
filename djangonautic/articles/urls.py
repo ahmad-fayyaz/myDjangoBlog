14afe0e92366c91dda7cae7c8a13547ba606c861
afayyaz1 <afayyaz1@swarthmore.edu>
2024-05-30 13:07:33 -0400

    setting up login authenticated content and proper redirections

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -5,5 +5,6 @@ app_name = 'articles'
 
 urlpatterns = [
     path('', views.article_list, name="list"),
+    path('create/', views.article_create, name="create"),
     re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail, name="detail"),
 ]
--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
@@ -12,3 +13,7 @@ def article_detail(request, slug):
 
     article = Article.objects.get(slug=slug)
     return render(request, 'articles/article_detail.html', { 'article': article })
+
+@login_required(login_url="/accounts/login/")
+def article_create(request):
+    return render(request, 'articles/article_create.html')
