2c0665868215324f9c2025b42aacf585b88e4355
afayyaz1 <afayyaz1@swarthmore.edu>
2024-05-30 10:47:15 -0400

    added url parameters

--- djangonautic/articles/urls.py
+++ djangonautic/articles/urls.py
@@ -1,6 +1,7 @@
-from django.urls import path
+from django.urls import path, re_path
 from . import views
 
 urlpatterns = [
     path('', views.article_list),
+    re_path(r'^(?P<slug>[\w-]+)/$', views.article_detail),
 ]
--- djangonautic/articles/views.py
+++ djangonautic/articles/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from .models import Article
+from django.http import HttpResponse
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
     return render(request, 'articles/article_list.html', {'articles': articles})
 
+
+def article_detail(request, slug):
+    return HttpResponse(slug)
