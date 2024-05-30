2c71a2c763c64e310e8ed0a0901b49c0b6a58e9f
afayyaz1 <afayyaz1@swarthmore.edu>
2024-05-30 13:32:15 -0400

    added create-article frontend and backend saves

--- djangonautic/articles/models.py
+++ djangonautic/articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 class Article(models.Model):
     title = models.CharField(max_length=100)
@@ -6,7 +7,8 @@ class Article(models.Model):
     body = models.TextField()
     date = models.DateTimeField(auto_now_add=True)
     thumb = models.ImageField(default='default.png', blank=True)
-    #add author later
+    author = models.ForeignKey(User,on_delete=models.CASCADE,default=None) 
+    
     def __str__(self):
         return self.title
     
