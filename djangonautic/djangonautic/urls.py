6c26618245e9dc1be45b7b5aba860884de78dbbd
afayyaz1 <afayyaz1@swarthmore.edu>
2024-05-30 14:04:01 -0400

    finished styling

--- djangonautic/djangonautic/urls.py
+++ djangonautic/djangonautic/urls.py
@@ -4,13 +4,14 @@ from . import views
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as articles_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('accounts/', include('accounts.urls')),
     path('about/', views.about),
-    path('', views.homepage),
+    path('', articles_views.article_list, name="home"),
 ]
 
 urlpatterns += staticfiles_urlpatterns()
